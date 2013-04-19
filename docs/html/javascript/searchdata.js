663f3131148bde6deb0d5076630363770b7c025a
Jay Kuri <jayk@ionzero.com>
2013-04-18 23:49:36 -0600

    Regen docs.
    Signed-off-by: Jay Kuri <jayk@ionzero.com>

--- docs/html/javascript/searchdata.js
+++ docs/html/javascript/searchdata.js
@@ -32,7 +32,7 @@ var indexSectionsWithContent = {
    "Functions": {
       "Symbols": true,
       "Numbers": false,
-      "A": false,
+      "A": true,
       "B": false,
       "C": true,
       "D": true,
