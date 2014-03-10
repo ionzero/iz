c0d5dc1af0389e5af8c5178e07c4480f1e5b6777
Jay Kuri <jayk@ionzero.com>
2014-03-10 12:46:28 -0600

    Make Module Load Errors more clear about what failed and how.
    Signed-off-by: Jay Kuri <jayk@ionzero.com>

--- test/iz_use.js
+++ test/iz_use.js
@@ -4,17 +4,17 @@ var assert = require('assert');
 var util = require('util');
 
 describe('IZ Use compatibility:', function () {
-	
+
 	before(function() {
-		
+
 	});
 
 	it('iz.Use calls callback properly', function(done) {
 		var callback_called = false;
 		this.timeout(500);
-		setTimeout(function() { 
-			assert.equal(callback_called, true); 
-			
+		setTimeout(function() {
+			assert.equal(callback_called, true);
+
 		}, 400);
 
 		var EvilDuck = iz.Use('Animals.EvilDuck', function() {
@@ -26,8 +26,8 @@ describe('IZ Use compatibility:', function () {
 	it('iz.Use called again calls callback', function(done2) {
 		var callback_called2 = false;
 		this.timeout(500);
-		setTimeout(function() { 
-			assert.equal(callback_called2, true); 
+		setTimeout(function() {
+			assert.equal(callback_called2, true);
 		}, 400);
 		var EvilDuck2 = iz.Use('Animals.EvilDuck', function() {
 			callback_called2 = true;
@@ -35,13 +35,25 @@ describe('IZ Use compatibility:', function () {
 		});
 	});
 
+	it('Gives a meaningful message on module error', function() {
+		var loaded = false;
+
+		try {
+			var Foo = iz.Use('Animals.BrokenDuck');
+		} catch (e) {
+			//console.log("FOO" + e.toString());
+			var r = new RegExp('SyntaxError', 'm');
+			assert.ok(r.test(e.toString()));
+		}
+
+	});
+
 	it('without search_path file outside path is not found', function() {
 		var failed = false;
 		var foo;
-		
+
 		try {
 			foo = iz.Use('FooClass');
-			console.log(foo);
 		} catch (e) {
 			failed = true;
 		}
@@ -49,16 +61,14 @@ describe('IZ Use compatibility:', function () {
 	});
 
 	it('uses search_path properly', function() {
-		var loaded = false;
 		iz.add_search_path('./outside_path/');
-		
+
 		var Foo = iz.Use('FooClass');
 		assert.equal(typeof Foo, 'function');
 		var foo = new Foo();
 		assert.equal(foo.foo(), 'foo');
 	});
 
-	
 	it('using lock_search_path prevents add_search_path from adding paths', function() {
 		var added = false;
 
