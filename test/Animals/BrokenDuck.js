57b9f84c281a8fcdb41b3f8a61288521ff756d81
Jay Kuri <jayk@ionzero.com>
2014-03-10 12:47:14 -0600

    Add BrokenDuck test file.
    Signed-off-by: Jay Kuri <jayk@ionzero.com>

---
+++ test/Animals/BrokenDuck.js
@@ -0,0 +1,11 @@
+var iz = require('../../lib/iz');
+//iz.use('Animals.Duck');
+
+module.exports = iz.Package('Animals.BrokenDuck', { extends: 'Animals.Duck' }, function (Class, SUPER) {
+
+   // THIS FILE IS INTENTIONALLY BROKEN TO PRODUCE A SYNTAX ERROR!
+   Class.has( 'quack', { default: 'Evil quack'}));
+
+
+   return Class;
+});
